a07d2bd0d8f65238f4b597039f4f98ef22c7c674
mreedul227 <khmreedul@gmail.com>
2022-05-10 22:07:23 +0600

    factory pattern added

--- src/App.java
+++ src/App.java
@@ -1,32 +1,71 @@
-class Circle{
-    public void draw(){
-        System.out.println("a circle drawn");
-    }
-}
-class Rectangle{
-    public void draw(){
-        System.out.println("a rectangle drawn");
-    }
-}
-class Square{
-    public void draw(){
-        System.out.println("a square drawn");
-    }
+package creational.pattern;
+
+interface Shape
+{
+	void draw();
 }
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        // Creating Object of Circle class 
-        Circle c = new Circle();
-        c.draw();
 
-        // Creating Object of Rectangle class 
-        Rectangle r = new Rectangle();
-        r.draw();
-        
-        // Creating Object of Square class 
-        Square s = new Square();
-        s.draw();
+class Circle implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a circle drawn");
+	 }
+}
 
-    }
+
+class Square implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a square drawn");
+	 }
+}
+
+class Rectangle implements Shape
+{
+	 @Override
+	 public void draw()
+	 {
+		 System.out.println("a rectangle drawn");
+	 }
+}
+
+
+class ShapeFactory
+{
+ 
+	 public Shape getShape(String type) throws Exception
+	 {
+		 switch (type)
+		 {
+			 case "Circle":
+				 return new Circle();
+			 case "Square":
+				 return new Square();
+			 case "Rectangle":
+				 return new Rectangle();
+			 default:
+				 throw new Exception( "Shape type : "+type+" cannot be instantiated");
+		 }
+	 }
+}
+
+class Painter
+{
+	 public static void main(String[] args) throws Exception
+	 {
+		 
+		 ShapeFactory shapeFactory = new ShapeFactory();
+		 Shape circle=shapeFactory.getShape("Circle");
+		 circle.draw();
+		 Shape square = shapeFactory.getShape("Square");
+		 square.draw();
+		 
+		 Shape rombus = shapeFactory.getShape("Rombus");
+		 rombus.draw();
+	 }
 }
