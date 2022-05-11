1cb9f41b4e4bf27bb26e82e275b09983e9f75970
akon171 <makon171154@bscse.uiu.ac.bd>
2022-05-11 23:33:25 +0600

    new function getarea() added

--- src/App.java
+++ src/App.java
@@ -1,30 +1,39 @@
-class Circle{
-    public void draw(){
+class Circle {
+    public void draw() {
         System.out.println("a circle drawn");
     }
 }
-class Rectangle{
-    public void draw(){
+
+class Rectangle {
+    public void draw() {
         System.out.println("a rectangle drawn");
     }
 }
-class Square{
-    public void draw(){
+
+class Square {
+    public void draw() {
         System.out.println("a square drawn");
+    }
+
+    public void getArea() {
+        int s = 20;
+        int area_square = s * s;
+        System.out.println("Area of the square=" + area_square);
+
     }
 }
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Creating Object of Circle class 
+        // Creating Object of Circle class
         Circle c = new Circle();
         c.draw();
 
-        // Creating Object of Rectangle class 
+        // Creating Object of Rectangle class
         Rectangle r = new Rectangle();
         r.draw();
-        
-        // Creating Object of Square class 
+
+        // Creating Object of Square class
         Square s = new Square();
         s.draw();
 
